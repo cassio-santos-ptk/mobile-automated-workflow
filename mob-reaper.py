bdabc74f753d54ded9846fd44bf5b06dfe5f1836
cassio-santos-ptk <cassio.santos@poatek.com>
2025-01-27 10:53:33 -0300

    add adb dev

--- mob-reaper.py
+++ mob-reaper.py
@@ -39,4 +39,4 @@ def execute_command(command):
         print("Error Output:", e.stderr)
 
 # Example: Execute a simple command
-execute_command(["ping", "-c", "1", "127.0.0.1"])  # Replace with your desired command
+execute_command(["adb", "devices"])  # Replace with your desired command
