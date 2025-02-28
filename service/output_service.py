884fd6c9c1d9d8ebb38595130f9a44d4861cad82
cassio-santos-ptk <cassio.santos@poatek.com>
2025-02-28 12:17:28 -0300

    add fix

--- service/output_service.py
+++ service/output_service.py
@@ -28,4 +28,4 @@ def build_output(rules, vulnerabilities):
     sarif_base['runs'][0]['results'] = vulnerabilities    
 
     with open("results.sarif", "w") as sarif_file:
-        json.dumps(sarif_base, sarif_file, indent=4)
+        json.dump(sarif_base, sarif_file, indent=4)
