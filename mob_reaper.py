c7f1806f56d76df69f7e758b659adfdf6c445d91
cassio-santos-ptk <cassio.santos@poatek.com>
2025-03-10 15:24:01 -0300

    add emulator

--- mob_reaper.py
+++ mob_reaper.py
@@ -41,10 +41,10 @@ def open_app():
     do_open(PACKAGE_NAME)        
 
     #check for root detection
-    vuln_service.check_root(PACKAGE_NAME)
+    #vuln_service.check_root(PACKAGE_NAME)
 
     #check for emulator detection
-    #vuln_service.check_emulator(PACKAGE_NAME)
+    vuln_service.check_emulator(PACKAGE_NAME)
 
     #androgoat_app.login_shared_pref_1()
 
@@ -58,10 +58,10 @@ def open_app():
 
     #do_restart(PACKAGE_NAME)
 
-    androgoat_app.login_insecure_logging()
+    #androgoat_app.login_insecure_logging()
         
     #search for sensitive logfed information - password
-    vuln_service.search_sensitive_log(shared.mock_data['password']) 
+    #vuln_service.search_sensitive_log(shared.mock_data['password']) 
 
     #@todo check the apk signature
 
