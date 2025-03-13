a71774dc171d5d1c236d6f02cc0f00c38368257c
cassio-santos-ptk <cassio.santos@poatek.com>
2025-03-13 15:39:17 -0300

    add sqlite full

--- service/db_service.py
+++ service/db_service.py
@@ -10,21 +10,28 @@ def search_for(table, cursor):
     cursor.execute(query)  
     return cursor.fetchall()
 
+def has_sensitive_data(data, sensitive_data):
+    # converts the db data to string and search for sensitive data
+    data_str = " ".join(map(str, data))
+
+    return sensitive_data in data_str
+
 def search_for_data(path, sensitive_data):
     try:
         conn = sqlite3.connect(path)
         cursor = conn.cursor()
 
+        # search for all the tables in de db
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = cursor.fetchall()
 
         if tables:
-            print("Tables in the db")
             for tb in tables:               
                 tb_name = tb[0]
                 content = search_for(tb_name, cursor)
-                print(" ---- content")
-                print(content)
+                if content:
+                    if has_sensitive_data(content, sensitive_data):                        
+                        return True, content                                
         else:
             print("No tables found in the db")
 
